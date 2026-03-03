b5179816e714c8fed799b6d5d10df3fc6c5154a7
Sanjeev-yadav <167008312+sanjeev-yd@users.noreply.github.com>
2026-03-04 00:35:18 +0530

    Update App.jsx

--- interviewIQ/client/src/App.jsx
+++ interviewIQ/client/src/App.jsx
@@ -10,7 +10,7 @@ import InterviewHistory from './pages/InterviewHistory'
 import Pricing from './pages/Pricing'
 import InterviewReport from './pages/InterviewReport'
 
-export const ServerUrl = "http://localhost:8000"
+export const ServerUrl = "https://interviewiq-ofi6.onrender.com"
 
 function App() {
 
